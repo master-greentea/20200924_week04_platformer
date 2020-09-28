a773e1f92dd7187dfd9d3e5079957a364736815e
Steven <yh3133@nyu.edu>
2020-09-28 23:07:58 +0800

    commit 1, basic inputs and scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D myRb;
+    float inputHori;
+    bool isJumping;
+    public bool isGrounded; // set by GroundedTigger.cs
+    public bool canDoubleJump;
+    public float moveSpeed = 10f;
+    public float jumpSpeed = 1f;
+
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        // movement horizontal with virtual input axis
+        inputHori = Input.GetAxis("Horizontal"); // return val from -1 to 1
+        
+        // jumping
+        if (Input.GetButtonDown("Jump") && isGrounded){
+            isJumping = true;
+        }
+        if (Input.GetButtonDown("Jump") && canDoubleJump) {
+            isJumping = true;
+            canDoubleJump = false;
+        }
+    }
+
+    // happens every physics frame, fixed timestep
+    void FixedUpdate() {
+        myRb.velocity = new Vector2(inputHori * moveSpeed, myRb.velocity.y);
+        
+        if (isJumping) {
+            myRb.velocity = new Vector2(myRb.velocity.x, jumpSpeed);
+            isJumping = false;
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectileLaunch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLaunch : MonoBehaviour
+{
+    public float rotationSpeed = 420f;
+    public float shootForce = 880f;
+    public Rigidbody2D Flame;
+    float minR = 1;
+    float maxR = 45;
+    // aiming cannon and left click to shoot
+    void Update()
+    {
+        // move mouse to aim
+        // -1f is move mouse left, 1f move mouse right, 0f not moving mouse horizontally
+        float vertiMouseSpeed = Input.GetAxis("Mouse Y");
+
+        // transform.Rotate(0, 0, horiMouseSpeed); // is framerate dependent
+        transform.Rotate(0, 0, vertiMouseSpeed * Time.deltaTime * rotationSpeed); // framerate independent
+        // dT usually a very small number between 0.0 - 1.0
+
+        // lock rotation?
+        Vector3 currentRotation = transform.localRotation.eulerAngles;
+        currentRotation.z = Mathf.Clamp(currentRotation.z, minR, maxR);
+        transform.localRotation = Quaternion.Euler(currentRotation);
+
+        // click to shoot ball
+        // instantiate ("clone") an object
+        if (Input.GetMouseButtonDown(0)) {
+            Rigidbody2D newClone = Instantiate(Flame, transform.position + transform.right, Quaternion.identity); //quaternion? 0, 0, 0, 0
+            // adding force to "down"
+            newClone.AddForce(transform.right * shootForce);
+        }
+    }
+}
